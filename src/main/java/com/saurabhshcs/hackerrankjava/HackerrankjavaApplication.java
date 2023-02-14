dae0ea882757ade15d5f07c1211925f4043b712e
saurabhshcs <saurabhshcs@gmail.com>
2023-02-14 23:23:30 +0000

    Commit project structure

---
+++ src/main/java/com/saurabhshcs/hackerrankjava/HackerrankjavaApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.hackerrankjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabhshcs/hackerrankjava/HackerrankjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabhshcs.hackerrankjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerrankjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
